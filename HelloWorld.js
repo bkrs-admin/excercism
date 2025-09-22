2eeee99dc141d7badb16ae0e8eade28e00031199
Jay <jayatbkrs@gmail.com>
2025-09-21 22:41:59 -0700

    hello world

---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+"use strict";
+function hello() {
+    return 'Hello, World!';
+}
+var a = hello();
+console.log(a);
---
+++ HelloWorld.ts
@@ -0,0 +1,6 @@
+function hello(): string {
+  return 'Hello, World!'
+}
+
+let a = hello()
+console.log(a)
