d61c23528c89b5fbcf39f1e96ee9f35810ef7418
Jay <jayatbkrs@gmail.com>
2025-09-21 22:44:08 -0700

    two fer from excercism

---
+++ TwoFer.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.twoFer = twoFer;
+function twoFer(name) {
+    // ^                 ^   ^ this is called a return type; it's the type of the
+    // ^                 ^     value that is returned from this function
+    // ^                 ^
+    // ^                 parameters go here
+    // ^
+    // allows the tests to import this function and call it
+    if (!name) {
+        name = "you";
+    }
+    return "One for ".concat(name, ", one for me.");
+}
