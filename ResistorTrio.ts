8b82386c3c6c024f95f189641ba22c39874e8f43
Jay <jayatbkrs@gmail.com>
2025-10-03 16:12:03 -0700

    resitor tri updated

--- ResistorTrio.ts
+++ ResistorTrio.ts
@@ -1,34 +1,55 @@
-export function decodedResistorValue(code: string[]): string {
-  const COLOR_MAP = {
-    'black': 0,
-    'brown': 1,
-    'red': 2,
-    'orange': 3,
-    'yellow': 4,
-    'green': 5,
-    'blue': 6,
-    'violet': 7,
-    'grey': 8,
-    'white': 9,
-  };
+export function decodedResistorValue(param: string[]): string {
+  const first: number = COLORS.indexOf(param[0]) * 10
+  const second: number = COLORS.indexOf(param[1])
+  const third: number = COLORS.indexOf(param[2])
 
-  const firstDigit = COLOR_MAP[code[0] as keyof typeof COLOR_MAP];
-  const secondDigit = COLOR_MAP[code[1] as keyof typeof COLOR_MAP];
-  const multiplier = COLOR_MAP[code[2] as keyof typeof COLOR_MAP];
-  
+  let total:number = (first + second) * exponetial(third)
 
-  let value = (firstDigit * 10 + secondDigit) * Math.pow(10, multiplier)
-  
-  if (value >= 1_000_000_000) {
-    return `${value / 1_000_000_000} gigaohms`;
-  }
-  if (value >= 1_000_000) {
-    return `${value / 1_000_000} megaohms`;
-  }
-  if (value >= 1_000) {
-    return `${value / 1_000} kiloohms`;
-  }
+  const res:string = digitToFormat(total)
 
-  return `${value} ohms`;
-  
+  return res
 }
+
+export function exponetial(index: number): number{
+  let res:number = 0;
+  
+  if(index === 0){
+    res = 1
+  } else {
+    res = 10 ** index
+  }
+  
+  return res 
+}
+
+export function digitToFormat(value: number): string{
+  const KILO = 1000
+  const MEGA = 1000000
+  const GIGA = 1000000000
+  let res: string = "";
+  
+  if (value >= KILO && value < MEGA){
+    res = `${value/1000} kiloohms`
+  } else if (value >= MEGA && value < GIGA){
+    res = `${value/1000000} megaohms`
+  } else if (value >= GIGA){
+    res = `${value/1000000000} gigaohms`
+  } else {
+    res = `${value} ohms`
+  }
+  
+  return res
+}
+
+export const COLORS: string[] = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
