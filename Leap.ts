42055f6bfa8e42f91442c0f0d0c84914122b667e
Jay <jayatbkrs@gmail.com>
2025-10-04 21:04:21 -0700

    leap year simplified

--- Leap.ts
+++ Leap.ts
@@ -1,19 +1,11 @@
-export function isLeap( year: number ):boolean {
-  let isLeap:boolean = false;
+export function isLeap(year: number): boolean {
+  let flag: boolean = true;
   
-  if (year % 4 == 0 && year % 400 == 0){
-    isLeap = true;
-  } else if (year % 4 == 0 && year % 100 != 0){
-    isLeap = true;
-  } else if (year % 4 == 0 && year % 5 == 0){
-    if ( year % 100 == 0 && year % 200 == 0 && year % 400 != 0){
-      isLeap = false;
-    } else if ( year % 100 == 0 && year % 3 != 0 || year % 400 != 0){
-      isLeap = false;
-    } else {
-      isLeap = true;
-    }
-  } 
-  
-  return isLeap;
+  if (year % 4 != 0){
+    flag = false;
+  } else if (year % 400 != 0 && year % 100 == 0){
+    flag = false;
+  }
+
+  return flag
 }
