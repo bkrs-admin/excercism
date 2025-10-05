c05837cd82bb74578761a1861aa7d4564ba5d9cd
Jay <jayatbkrs@gmail.com>
2025-10-04 21:46:58 -0700

    space age updated

--- SpaceAge.ts
+++ SpaceAge.ts
@@ -1,23 +1,36 @@
 export function age(planet: string, seconds: number): number {
-  let year:number = 0;
   
-  year = seconds / 31557600;
+  const earthYear:number = secondsToYear(seconds);
+  const planetYear: number = toPlanetYear(planet, earthYear);
   
-    if (planet === "mercury"){
-      year = year / 0.2408467
-    } else if (planet === "venus"){
-      year = year / 0.61519726
-    } else if (planet === "mars"){
-      year = year / 1.8808158
-    } else if (planet === "jupiter"){
-      year = year / 11.862615
-    } else if (planet === "saturn"){
-      year = year / 29.447498
-    } else if (planet === "uranus"){
-      year = year / 84.016846
-    } else if (planet === "neptune"){
-      year = year / 164.79132
-    }
+  return planetYear;
+}
 
-  return Number(year.toFixed(2))
+export function secondsToYear(sec: number): number {
+  let year: number = sec / 31557600;
+
+  return year;
+}
+
+export function toPlanetYear(planet: string, earthYear: number): number {
+  let planetYear:number = 0;
+
+  if (planet == 'mercury'){
+    planetYear = earthYear * (1 / 0.2408467);
+  } else if (planet == 'venus'){
+    planetYear = earthYear * (1 / 0.61519726);
+  } else if (planet == 'mars'){
+    planetYear = earthYear * (1 / 1.8808158);
+  } else if (planet == 'jupiter'){
+    planetYear = earthYear * (1 / 11.862615);
+  } else if (planet == 'saturn'){
+    planetYear = earthYear * (1 / 29.447498);
+  } else if (planet == 'uranus'){
+    planetYear = earthYear * (1 / 84.016846);
+  } else if (planet == 'neptune'){
+    planetYear = earthYear * (1 / 164.79132);
+  } else {
+    planetYear = earthYear;
+  }
+  return Number(planetYear.toFixed(2));
 }
